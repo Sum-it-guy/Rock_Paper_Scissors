58d364bb23eac9162ded7d684a921acb88106d40
sumit <bartwalsumit860@gmail.com>
2025-08-27 22:28:13 +0530

    rewrite the logic of geting computer choices
    erase the previouse logic entirly to wire new logic with event listeners

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 const start_btn = document.querySelector(".start_btn");
+const y_choice = document.querySelector(".y_choice");
+const c_choice = document.querySelector(".c_choice");
+
+//logic of getting computer choices
+
+function getComputerChoices(){
+    let value = Math.floor(Math.random()*3);
+    console.log(value);
+
+    if(value == 0){
+        c_choice.textContent ="Rock";
+    }
+
+    else if(value == 1){
+        c_choice.textContent ="Paper";
+    }
+
+    else{
+        c_choice.textContent="Scissors";
+    }
+}
+
+start_btn.addEventListener("click",getComputerChoices);
 
 
 
