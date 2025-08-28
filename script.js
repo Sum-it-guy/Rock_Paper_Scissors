b415eaa46e5563f4bb693b8b41fdcf2574c07015
sumit <bartwalsumit860@gmail.com>
2025-08-28 22:36:19 +0530

    write the logic of playing the game
    a rough logic of how actually scores are given

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const start_btn = document.querySelector(".start_btn");
 const y_choice = document.querySelector(".y_choice");
 const c_choice = document.querySelector(".c_choice");
 
+//setting up global values 
+let playerScore = 0;
+let ComputerScore = 0;
+let human_choice =null;
 
 //logic of getting computer choices
 
@@ -16,27 +20,78 @@ function getComputerChoices(){
 
     if(value == 0){
         c_choice.textContent ="Rock";
+        return "rock";
     }
 
     else if(value == 1){
         c_choice.textContent ="Paper";
+        return "paper";
     }
 
     else{
         c_choice.textContent="Scissors";
+        return "scissors";
     }
 }
 
 //getting human choices 
 
 function get_human_choices(){
-     rock.addEventListener("click",()=>y_choice.textContent = "Rock");
-     paper.addEventListener("click",()=>y_choice.textContent = "Paper");
-     scissors.addEventListener("click",()=>y_choice.textContent = "Scissors");
+     rock.addEventListener("click",()=>{y_choice.textContent = "Rock";
+                                            human_choice  = "rock";
+                                        });
+     paper.addEventListener("click",()=>{y_choice.textContent = "Paper";
+                                        human_choice =  "paper";
+     });
+     scissors.addEventListener("click",()=>{y_choice.textContent = "Scissors";
+                                        human_choice = "scissors";
+     });
 }
 
 
+//playground functon 
 
+
+
+
+function play(human,computer){
+
+    
+
+     if(human == "rock" && computer == "paper"){
+        ComputerScore++;
+    }
+    
+
+    else if(human == "paper" && computer == "scissors"){
+        ComputerScore++;
+    }
+
+    else if(human == "scissors" && computer == "rock"){
+        ComputerScore++;
+    }
+
+    else if(human == "rock" && computer == "scissors"){
+        playerScore++;
+    }
+
+    else if(human == "paper" && computer == "rock"){
+        playerScore++;
+    }
+
+    else if(human == "scissors" && computer == "paper"){
+        playerScore++;
+    }
+
+    your_score.textContent =playerScore;
+    computer_score.textContent = ComputerScore;
+
+}
+
+start_btn.addEventListener("click",()=>{
+    let co = getComputerChoices();
+    play(human_choice,co);
+});
 
 
 
